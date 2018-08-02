25fc52d19e56998d638fba4fe27dd944f0479aa1
DePaul <CONF0205@depaul.edu>
2018-08-02 11:30:51 -0500

    TestCondition

--- TestCondition.js
+++ TestCondition.js
@@ -136,16 +136,9 @@
 		}
 		
 		
-	
-		$.ajax({
-			type: "GET",
-			url: "https://api.petfinder.com/pet.find?format=xml&key=b1773aef3c7bb7e15eb73e500fb5919c&animal=bird&location=60657",
-			success: function(result){
-				$("#div1").html(result);
-			},
-			crossDomain: true
-		});
-	
+		
+		var url = "https://api.petfinder.com/pet.find?format=xml&key=b1773aef3c7bb7e15eb73e500fb5919c&animal=bird&location=60657";
+		makeCorsRequest(url);
 		
 				
 		console.log(result)
@@ -153,5 +146,46 @@
 		$("#animal").html(result);
 
 	  });
+	  
+	// Create the XHR object.
+	function createCORSRequest(method, url) {
+	  var xhr = new XMLHttpRequest();
+	  if ("withCredentials" in xhr) {
+		// XHR for Chrome/Firefox/Opera/Safari.
+		xhr.open(method, url, true);
+	  } else if (typeof XDomainRequest != "undefined") {
+		// XDomainRequest for IE.
+		xhr = new XDomainRequest();
+		xhr.open(method, url);
+	  } else {
+		// CORS not supported.
+		xhr = null;
+	  }
+	  return xhr;
+	}
+
+	// Make the actual CORS request.
+	function makeCorsRequest(url) {
+	  // This is a sample server that supports CORS.
+
+	  var xhr = createCORSRequest('GET', url);
+	  if (!xhr) {
+		alert('CORS not supported');
+		return;
+	  }
+
+	  // Response handlers.
+	  xhr.onload = function() {
+		var text = xhr.responseText;
+		//var title = getTitle(text);
+		alert('Response from CORS request to ' + url);
+	  };
+
+	  xhr.onerror = function() {
+		alert('Woops, there was an error making the request.');
+	  };
+
+	  xhr.send();
+	}
 	  
   });
